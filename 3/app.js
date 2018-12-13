2070899b862c1c38f5f25c9e30f6e05409733219
Ultre <kevin.henzen@gmail.com>
2018-12-13 23:05:50 +0100

    day 3 part 2

--- 3/app.js
+++ 3/app.js
@@ -13,24 +13,52 @@ createField = (size) => {
     return field;
 }
 
-let field = createField(1000);
-
-const run = async () => {
-    const data = await utils.readFile('./input');
+const parseData = (data) => {
+    let result = [];
     data.forEach(row => {
         const start = row.indexOf("@");
         const end = row.indexOf(":");
+        const id = Number(row.substring(1, start))
         const xy = row.substring(start + 2, end).split(',').map(m => Number(m));
         const size = row.substring(end + 2, row.length).split('x').map(m => Number(m));
-        for (let x = 0; x < size[0]; x++) {
-            for (let y = 0; y < size[1]; y++) {
-                field[x + xy[0]][y + xy[1]]++;
+        result.push({ id, x: xy[0], y: xy[1], width: size[0], height: size[1] })
+    });
+    return result;
+}
+
+const fillField = (field, data) => {
+    for (let i = 0; i < data.length; i++) {
+        const row = data[i];
+        for (let x = 0; x < row.width; x++) {
+            for (let y = 0; y < row.height; y++) {
+                field[x + row.x][y + row.y]++;
             }
         }
-    });
-    console.log(field.reduce((result, cur) => result + cur.reduce((result2, cur2) => result2 + (cur2 >= 2 ? 1 : 0), 0), 0));
+    }
+}
 
-    const fs = require('fs');
-    fs.writeFile("./output", field.reduce((result, cur) => result + cur.join("") + '\n', ""), err => { });
+const findNonOverlappingId = (field, data) => {
+    for (let i = 0; i < data.length; i++) {
+        const row = data[i];
+        let nonOverlapping = true
+        for (let x = 0; x < row.width; x++) {
+            for (let y = 0; y < row.height; y++) {
+                if (field[x + row.x][y + row.y] > 1)
+                    nonOverlapping = false;
+            }
+        }
+        if(nonOverlapping){
+            console.log(row.id)
+            break;
+        }
+    }
+}
+
+const run = async () => {
+    const rawData = await utils.readFile('./input');
+    const data = parseData(rawData);
+    let field = createField(1000);
+    fillField(field, data);
+    findNonOverlappingId(field, data);
 }
 run();
