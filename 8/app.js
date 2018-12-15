bdc18bb881c801f568651f0fceece1892d4f6908
Ultre <kevin.henzen@gmail.com>
2018-12-15 22:57:34 +0100

    day 8 part 2

--- 8/app.js
+++ 8/app.js
@@ -4,7 +4,7 @@ const parseData = input => input[0].split(" ").map(m => Number(m))
 
 const toNode = (data) => {
     const header = data.splice(0, 2);
-    let node = { }    
+    let node = {}
 
     for (let i = 0; i < header[0]; i++) {
 
@@ -21,11 +21,22 @@ const countMetadataEntries = node => {
     return (node.nodes ? node.nodes.reduce((result, cur) => result + countMetadataEntries(cur), 0) : 0) + node.metadataEntries.reduce((result, cur) => result + cur)
 };
 
+const countMetadataEntriesPart2 = node => {
+    if (node.nodes) {
+        return node.metadataEntries.filter(m => m > 0).map(m => m - 1).filter(m => node.nodes[m]).reduce((result, cur) => {
+            return result + countMetadataEntriesPart2(node.nodes[cur]);
+        }, 0);
+    } else {
+        return node.metadataEntries.reduce((result, cur) => result + cur);
+    }
+    return 0;
+}
+
 const run = async () => {
     let input = await utils.readFile('./input');
     const data = parseData(input);
     const root = toNode(data);
-    const result = countMetadataEntries(root);
+    const result = countMetadataEntriesPart2(root);
     console.log(result);
 }
 run();
